020b52d7d365703d1cada101ce5aeba4571ce6c2
Illyya <62709522+Illyya@users.noreply.github.com>
2020-09-30 18:50:16 +0300

    поменял "доб, задачу" на "+", добавил зачеркивание

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 let input = document.querySelector('.todo__input');
 let addTask = document.querySelector('.todo__btn_add');
 let list = document.querySelector('.todo__list');
-let removeTask = document.querySelector('.todo__btn_remove');
+let deleteTasks = document.querySelector('.todo__btn_remove');
 
 addTask.onclick = function (e) {
     e.preventDefault();
     if (input.value) {
         let liFirst = document.createElement('li');
-        let cross = document.createElement('span');
-        liFirst.textContent = input.value;
-        cross.innerHTML = '&#10006';
+        let par = document.createElement('p');
+        let done = document.createElement('span');
+        let deleteTask = document.createElement('span');
+        par.textContent = input.value;
+        done.innerHTML = '&#10004';
+        done.style.marginLeft = 'auto';
+        done.style.marginRight = '0.7rem';
+        done.style.color = 'green';
+        deleteTask.innerHTML = '&#10006';
         list.prepend(liFirst);
-        liFirst.append(cross);
+        liFirst.prepend(par);
+        liFirst.append(done);
+        liFirst.append(deleteTask);
         input.value = '';
-        cross.onclick = function() {
+        deleteTask.onclick = function() {
             liFirst.remove();
+        }
+        done.onclick = function() {
+            par.classList.toggle('line-through');            
         }
     }
 }
-removeTask.onclick = function (e) {
+deleteTasks.onclick = function (e) {
     e.preventDefault();
     let tasks = document.querySelectorAll('li');
     for (let i = 0; i < tasks.length; i++) {
